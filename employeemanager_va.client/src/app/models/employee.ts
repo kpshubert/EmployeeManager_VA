5d51231eb4310d0cde882314840d9c6f956a880c
Kurt Shubert <kpshubert@hotmail.com>
2025-08-12 13:50:21 -0500

    20250812 Custom Input Control First Scald

--- employeemanager_va.client/src/app/models/employee.ts
+++ employeemanager_va.client/src/app/models/employee.ts
@@ -1,12 +1,12 @@
 export interface Employee {
-  rowNum: number,
-  id: number,
-  firstName: string,
-  lastName: string,
-  phone: string,
-  email: string,
-  departmentId: number,
-  departmentIdString: string,
-  departmentName: string,
+  [rowNum: string]: number | string;
+  id: number;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  departmentId: number;
+  departmentIdString: string;
+  departmentName: string;
   formMode: string
 }
