fa7bf1407891e29cb73008b6156b323ffed39ba8
Kurt Shubert <kpshubert@hotmail.com>
2025-08-25 12:39:45 -0500

    20250825 - Add Loading Message to Employee and Department Screens. Fix issue with last deparment deleted not changing display. Fix various minor issues.

---
+++ employeemanager_va.client/src/app/Shared/Modules/loading-data/loading-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-data',
+  standalone: false,
+  templateUrl: './loading-data.component.html',
+  styleUrl: './loading-data.component.css'
+})
+export class LoadingDataComponent {
+
+}
--- employeemanager_va.client/src/app/app.module.ts
+++ employeemanager_va.client/src/app/app.module.ts
@@ -3,19 +3,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routes } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatProgressBar, MatProgressBarModule } from '@angular/material/progress-bar';
+import { OverlayModule } from '@angular/cdk/overlay'; import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
 import Lara from '@primeuix/themes/aura';
 import * as $ from "jquery";
 import { AppComponent } from './Views/app/app.component';
+import { LoadingDataComponent } from '..//../src/app/Shared/Modules/loading-data/loading-data.component';
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [BrowserModule,
     AppRoutingModule,
     AppComponent,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatProgressBarModule,
+    OverlayModule
   ],
   providers: [provideHttpClient(withFetch(),
     withInterceptorsFromDi()),
@@ -26,6 +34,9 @@ import { AppComponent } from './Views/app/app.component';
       }
     }),
     provideRouter(routes)
+  ],
+  declarations: [
+    LoadingDataComponent
   ]
 })
 export class AppModule {
